636f63d6e7edf324f578309e5210c1479742b376
MiriamGonzalez08 <72994216+MiriamGonzalez08@users.noreply.github.com>
2022-03-22 12:19:55 -0600

    Add files via upload

---
+++ ConversorDecimal/ConversorDecimal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Conversor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("**** DECIMAL A BINARIO ****");
+            Console.WriteLine("Ingrese un número entero");
+            int numDecimal = Convert.ToInt32(Console.ReadLine());
+            string numBinario = "";
+
+                           
+            while (numDecimal > 0)
+            {
+                if (numDecimal % 2 == 0)
+                {
+                    numBinario = "0" + numBinario;
+                }
+                else
+                {
+                    numBinario = "1" + numBinario;
+                }
+                numDecimal = numDecimal / 2;
+            }
+            Console.WriteLine(numBinario);
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
